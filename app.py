fb0f145ee715ede00f8f5b1b2d88a792a6af41ce
Cristianpardoluna <55992535+CristianCamiloPardoLuna@users.noreply.github.com>
2021-03-24 23:55:26 -0500

    content

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ def index():
         'index.html'
         )
 
+@APP.route('/pricing')
+def pricing():
+    return render_template(
+        'pricing.html'
+    )
+
+@APP.route('/contact')
+def contact():
+    return render_template(
+        'contactus.html'
+    )
+
 
 if __name__ == '__main__':
     APP.debug = True
