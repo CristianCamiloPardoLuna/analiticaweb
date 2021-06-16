f41c02edd609d825e537cab30115a68fa835db2e
Cristianpardoluna <55992535+CristianCamiloPardoLuna@users.noreply.github.com>
2021-06-16 13:14:32 -0500

    robots.txt

--- app.py
+++ app.py
@@ -66,8 +66,7 @@ def plans(_plan):
         tellusmore = tellusmore
     )
 
-
-@APP.route('/contact')
+@APP.route('/contact/')
 def contact():
     return render_template(
         'contactus.html',
@@ -115,36 +114,36 @@ def contactme():
         'thanks.html'
     )
 
-@APP.route('/thanks')
+@APP.route('/thanks/')
 def thanks():
     return render_template(
         'thanks.html'
     )
 
-@APP.route('/underconstruction')
+@APP.route('/underconstruction/')
 def uc():
     return render_template(
         'underconstruction.html'
     )
 
-@APP.route('/packageinfo')
+@APP.route('/packageinfo/')
 def packageinfo():
     return render_template(
         'packageinfo.html'
     )
 
-@APP.route('/privacypolicy')
+@APP.route('/privacypolicy/')
 def privacypolicy():
     return render_template(
         'privacy.html'
     )
 
-@APP.route('/terms')
+@APP.route('/terms/')
 def terms():
     return render_template(
         'termsconditions.html'
     )
 
 if __name__ == '__main__':
-    APP.debug = False
+    APP.debug = True
     APP.run()
